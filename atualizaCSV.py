99e45d84d37793c2c4628814d80a03a4929d6b21
rafael-valiati <44684423+rafael-valiati@users.noreply.github.com>
2024-11-27 10:26:38 -0300

    Update atualizaCSV.py
    Correção de erro

--- atualizaCSV.py
+++ atualizaCSV.py
@@ -19,7 +19,7 @@ def get_weather_data():
         raise ValueError("A chave da API não foi encontrada. Defina 'WU_API_KEY' como uma variável de ambiente.")
     STATION_ID = "ISOPAU314"
     url = f"https://api.weather.com/v2/pws/observations/current?stationId={STATION_ID}&format=json&units=m&numericPrecision=decimal&apiKey={WU_API_KEY}"
-    timestamp = datetime.strptime(obs_time_utc, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc).astimezone(brasilia_tz)
+    timestamp = datetime.now(tz=brasilia_tz)
     try:
         response = requests.get(url)
         response.raise_for_status()
